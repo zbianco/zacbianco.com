46286e0134476743401663b637439041fe497188
Zac Bianco <zac@zacbianco.com>
2026-08-04 13:47:16 -0500

    remove redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  },
-  redirects: {
-    'ontap': 'on-tap',
-    'tap': 'on-tap',
-    'taplist': 'on-tap',
-    'tap-list': 'on-tap'
   }
 });
